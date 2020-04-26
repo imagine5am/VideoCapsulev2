da6b7eba37c616ed12bb0feff3c8ad6c8797c40c
SHIVAM SOOD <ssood@cse.iitb.ac.in>
2020-04-26 22:14:32 +0530

    Correction in correlation

--- correlation.py
+++ correlation.py
@@ -9,7 +9,7 @@ from scipy.misc import imread
 dataset_dir = '../SynthVideo/out/'
 
 def preprocess():
-    video_dir = dataset_dir + "Frames/" + '8/16916_tilt1_'
+    video_dir = dataset_dir + "Frames/" + '8/16916_tilt1_/'
     n_frames = len(os.listdir(video_dir))
     video = np.zeros((n_frames, config.vid_h, config.vid_w, 3), dtype=np.uint8)
     for idx in range(n_frames):
@@ -24,6 +24,10 @@ def preprocess():
 def corr(x_batch):
     capsnet = Caps3d()
     with tf.Session(graph=capsnet.graph, config=config.gpu_config) as sess:
+        print('All layers:')
+        op = sess.graph.get_operations()
+        print([m.values() for m in op][1])
+        
         tf.global_variables_initializer().run()
         capsnet.load(sess, config.network_save_dir)
         sec_caps_out = sess.run([sess.graph.get_tensor_by_name('sec_caps')], 
