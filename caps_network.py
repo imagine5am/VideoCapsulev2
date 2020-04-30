ffc40d5377c7207a8b7afb403d71755b39cabe9e
SHIVAM SOOD <ssood@cse.iitb.ac.in>
2020-04-30 23:55:03 +0530

    For writing meta graph

--- caps_network.py
+++ caps_network.py
@@ -134,7 +134,8 @@ class Caps3d(object):
         n_classes, dim = map(int, [n_classes, dim])
 
         # masks the capsules that are not the ground truth (training) or the prediction (testing)
-        vec_to_use = tf.cond(tf.math.logical_or(self.is_train, self.is_real), lambda: self.y_input, lambda: self.predictions)
+        # vec_to_use = tf.cond(tf.math.logical_or(self.is_train, self.is_real), lambda: self.y_input, lambda: self.predictions)
+        vec_to_use = tf.cond(self.is_train, lambda: self.y_input, lambda: self.predictions)
         vec_to_use = tf.one_hot(vec_to_use, depth=n_classes)
         vec_to_use = tf.tile(tf.reshape(vec_to_use, (batch_size, n_classes, 1)), multiples=[1, 1, dim])
         masked_caps = pred_caps_poses * tf.cast(vec_to_use, dtype=tf.float32)
@@ -402,7 +403,7 @@ class Caps3d(object):
 
     def save(self, sess, file_name, ep):
         # saves the model
-        save_path = self.saver.save(sess, file_name, global_step=ep, write_meta_graph=False)  # +25
+        save_path = self.saver.save(sess, file_name, global_step=ep, write_meta_graph=True)  # +25
         # save_path = self.saver.save(sess, file_name)
         print("Model saved in file: %s" % save_path)
 
