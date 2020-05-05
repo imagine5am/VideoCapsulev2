17a3966c2f4e84df5c4e76693145f9cf0e86402f
SHIVAM SOOD <ssood@cse.iitb.ac.in>
2020-05-05 16:47:54 +0530

    Change in config

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ data_type = 'real' # 'real', 'synth'
 
 # whether to continue from last checkpoint or not
 continue_from_chkpt = True
-prev_epochs = 34
+prev_epochs = 35
 
 ann_types = ['para_ann', 'line_ann', 'word_ann', 'char_ann']
 
@@ -34,7 +34,7 @@ acc_for_eval = 0.5
 n_eps_until_eval = 0
 
 # learning rate and beta1 are used in the Adam optimizer.
-learning_rate, beta1 = 0.0001, 0.5
+learning_rate, beta1 = 0.001, 0.5
 
 # Used to prevent numerical instability (dividing by zero or log(0))
 epsilon = 1e-6
